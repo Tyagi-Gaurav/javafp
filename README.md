64fcb9d66903acc4b9a771f90a354f60105e299a
Tyagi-Gaurav <aanconsultancyservicesltd@gmail.com>
2021-06-24 14:22:24 +0100

    All list functions with readme

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # javafp
 [![Java CI with Gradle](https://github.com/Tyagi-Gaurav/javafp/actions/workflows/gradle.yml/badge.svg)](https://github.com/Tyagi-Gaurav/javafp/actions/workflows/gradle.yml)
 
+## List Operations
+
+| Operation     | Description     |  
+| :-------------      | :----------:      | 
+| Head  | Returns the first element of the list   |
+| Tail  | Everything after the first element of the list   |
+| Init  | Everything but the last element of the list   |
+| Reverse  | Self descriptive   |
+| Take While  | Return the first `x` elements of a list  |
+| Drop While  | Return the elements of list after skipping the first `x` elements  |
+| Sum  | Compute the sum of all the elements of the list  |
+
+### Head
+Returns the head of the list
+
+### Tail
+
+## Exceptions
+
 ### Try Catch With no return Value
 * Use this block when you like to execute a routine without expecting any 
 return value.
---
+++ core/src/main/java/com/gt/jfp/construct/collection/ListUtils.java
@@ -0,0 +1,134 @@
+package com.gt.jfp.construct.collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.ToDoubleFunction;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+
+public class ListUtils {
+    /**
+     *
+     * @param input
+     * @return First element of the input
+     * @throws IllegalArgumentException if the input is empty or null
+     */
+    public static <T> T headOf(List<T> input) {
+        if (Objects.isNull(input) || input.isEmpty())
+            throw new IllegalArgumentException();
+
+        return input.get(0);
+    }
+
+    /**
+     *
+     * @param input
+     * @return Everything after the first element of the input
+     * @throws IllegalArgumentException if the input is empty or null
+     */
+    public static <T> List<T> tailOf(List<T> input) {
+        if (Objects.isNull(input) || input.isEmpty())
+            throw new IllegalArgumentException();
+
+        if (input.size() == 1)
+            return Collections.emptyList();
+        else {
+            return IntStream.rangeClosed(1, input.size() - 1)
+                    .mapToObj(input::get)
+                    .collect(Collectors.toList());
+        }
+    }
+
+    /**
+     * Return everything but the last element of the input
+     *
+     * @param input
+     * @return Everything but the last element of the list
+     * @throws IllegalArgumentException if the input is empty or null
+     */
+    public static <T> List<T> InitOf(List<T> input) {
+        if (Objects.isNull(input) || input.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        if (input.size() == 1)
+            return Collections.emptyList();
+        else {
+            return IntStream.range(0, input.size() - 1)
+                    .mapToObj(input::get)
+                    .collect(Collectors.toList());
+        }
+    }
+
+    /**
+     * Reverses a list
+     *
+     * @param input
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> Reverse(List<T> input) {
+        if (input.size() == 0)
+            return Collections.emptyList();
+        else {
+            List<T> output = new ArrayList<>(Reverse(tailOf(input)));
+            output.add(headOf(input));
+            return output;
+        }
+    }
+
+    /**
+     * Return the first <code>count</code> elements from the list
+     *
+     * @param count
+     * @param input
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> Take(int count, List<T> input) {
+        if (input == null || input.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        return input.stream().limit(Math.min(count, input.size()))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Return the elements of list after dropping the first
+     * <code>count</code> elements from the list.
+     *
+     * @param count
+     * @param input
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> Drop(int count, List<T> input) {
+        if (input == null || input.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        return input.stream().skip(Math.min(count, input.size()))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Return the sum of all the elements of a list
+     *
+     * @param input
+     * @param <T>
+     * @return
+     */
+    public static <T extends Number> double Sum(List<T> input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        return input.stream().mapToDouble(Number::doubleValue).sum();
+    }
+}
