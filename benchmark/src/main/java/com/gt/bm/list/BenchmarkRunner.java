8219668603df43da10894b6d9e3b3dd4f616b17a
Tyagi-Gaurav <aanconsultancyservicesltd@gmail.com>
2021-07-08 11:28:46 +0100

    Benchmark for Foldl

---
+++ benchmark/src/main/java/com/gt/bm/list/BenchmarkRunner.java
@@ -0,0 +1,11 @@
+package com.gt.bm.list;
+
+import org.openjdk.jmh.Main;
+
+import java.io.IOException;
+
+public class BenchmarkRunner {
+    public static void main(String[] args) throws IOException {
+        Main.main(args);
+    }
+}
